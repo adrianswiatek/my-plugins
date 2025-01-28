dcad64b98971e367599f1d6eea9ff6e990b826dc
Adrian Swiatek <adrian@aswiatek.pl>
2025-01-28 18:30:42 +0100

    Initial Commit

---
+++ MyPlugins/MyPluginsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPluginsApp.swift
+//  MyPlugins
+//
+//  Created by Adrian Świątek on 28/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct MyPluginsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyPluginsTests/MyPluginsTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyPluginsTests.swift
+//  MyPluginsTests
+//
+//  Created by Adrian Świątek on 28/01/2025.
+//
+
+import Testing
+@testable import MyPlugins
+
+struct MyPluginsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
